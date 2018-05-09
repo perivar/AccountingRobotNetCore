682b6aae128f631c012e5f17074377d1f05a2832
Per Ivar Nerseth <perivar@nerseth.com>
2018-05-10 00:06:50 +0200

    Added PayPal Tranastion search

--- AccountingServices/Paypal.cs
+++ AccountingServices/Paypal.cs
@@ -16,10 +16,7 @@ namespace AccountingServices
         public static List<PayPalTransaction> GetPayPalTransactions(IMyConfiguration configuration, DateTime from, DateTime to)
         {
             //var transaction = GetPayPalTransactionsList(configuration, from, to);
-            var transaction = GetPayPalTransactionsList(configuration, DateTime.Now.AddDays(-31), DateTime.Now);
-
-            var payPalTransactions = new List<PayPalTransaction>();
-            return payPalTransactions;
+            return GetPayPalTransactionsList(configuration, DateTime.Now.AddDays(-31), DateTime.Now);
         }
 
         private static string GetAccessToken(IMyConfiguration configuration)
@@ -78,9 +75,11 @@ namespace AccountingServices
             var accessToken = JsonConvert.DeserializeObject<PayPalAccessToken>(content);
             return accessToken;
         }
-        
+
         private static List<PayPalTransaction> GetPayPalTransactionsList(IMyConfiguration configuration, DateTime from, DateTime to)
         {
+            var payPalTransactions = new List<PayPalTransaction>();
+
             try
             {
                 Task.Run(async () =>
@@ -91,8 +90,7 @@ namespace AccountingServices
                     var accessToken = await GetPayPalAccessTokenAsync(configuration, httpClient);
 
                     // Step 2: List the transactions
-                    var transacions = await GetPayPalTransactionsAsync(httpClient, accessToken, from, to);
-
+                    payPalTransactions = await GetPayPalTransactionsAsync(httpClient, accessToken, from, to);
                 }).Wait();
             }
             catch (Exception e)
@@ -100,11 +98,12 @@ namespace AccountingServices
                 Console.WriteLine("ERROR: Could not get paypal transactions! '{0}'", e.Message);
             }
 
-            return null;
+            return payPalTransactions;
         }
-
-        private static async Task<string> GetPayPalTransactionsAsync(HttpClient httpClient, PayPalAccessToken accessToken, DateTime from, DateTime to)
+        private static async Task<List<PayPalTransaction>> GetPayPalTransactionsAsync(HttpClient httpClient, PayPalAccessToken accessToken, DateTime from, DateTime to)
         {
+            var payPalTransactions = new List<PayPalTransaction>();
+
             string startDate = string.Format("{0:yyyy-MM-ddTHH\\:mm\\:ssZ}", from);
             string endDate = string.Format("{0:yyyy-MM-ddTHH\\:mm\\:ssZ}", to);
             string url = $"https://api.paypal.com/v1/reporting/transactions?fields=all&page_size=100&page=1&start_date={startDate}&end_date={endDate}";
@@ -112,12 +111,53 @@ namespace AccountingServices
             HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, url);
             request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken.access_token);
             HttpResponseMessage response = await httpClient.SendAsync(request);
-            string content = await response.Content.ReadAsStringAsync();
+            string jsonString = await response.Content.ReadAsStringAsync();
+            dynamic jsonD = JsonConvert.DeserializeObject(jsonString);
 
-            //httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken.access_token);
-            //var responseString = await httpClient.GetStringAsync(url);
+            // parse json 
+            string accountNumber = jsonD.account_number;
+            int total_pages = jsonD.total_pages;
+            DateTime dateStart = jsonD.start_date;
+            DateTime dateEnd = jsonD.end_date;
+            int page = jsonD.page;
+            int totalItems = jsonD.total_items;
 
-            return content;
+            foreach (var transaction in jsonD.transaction_details)
+            {
+                var transactionInfo = transaction.transaction_info;
+                var transactionId = transactionInfo.transaction_id;
+                string transactionEventCode = transactionInfo.transaction_event_code;
+                DateTime transactionInitiationDate = transactionInfo.transaction_initiation_date;
+                DateTime transactionUpdatedDate = transactionInfo.transaction_updated_date;
+                var transactionAmountObject = transactionInfo.transaction_amount;
+                string transactionAmountCurrencyCode = transactionAmountObject.currency_code;
+                decimal transactionAmountValue = transactionAmountObject.value;
+                string transactionStatus = transactionInfo.transaction_status;
+                string bankReferenceId = transactionInfo.bank_reference_id;
+                var endingBalanceObject = transactionInfo.ending_balance;
+                string endingBalanceCurrencyCode = endingBalanceObject.currency_code;
+                decimal endingBalanceValue = endingBalanceObject.value;
+                var availableBalanceObject = transactionInfo.available_balance;
+                string availableBalanceCurrencyCode = availableBalanceObject.currency_code;
+                decimal availableBalanceValue = availableBalanceObject.value;
+                string protectionEligibility = transactionInfo.protection_eligibility;
+
+                PayPalTransaction payPalTransaction = new PayPalTransaction();
+                payPalTransaction.TransactionID = transactionId;
+                payPalTransaction.Timestamp = transactionInitiationDate;
+                payPalTransaction.Status = transactionStatus;
+                payPalTransaction.Type = transactionEventCode;
+                payPalTransaction.GrossAmount = transactionAmountValue;
+                payPalTransaction.NetAmount = 0;
+                payPalTransaction.FeeAmount = 0;
+
+                // payPalTransaction.Payer = transaction.Payer;
+                // payPalTransaction.PayerDisplayName = transaction.PayerDisplayName;
+
+                payPalTransactions.Add(payPalTransaction);
+            }
+
+            return payPalTransactions;
         }
     }
 
