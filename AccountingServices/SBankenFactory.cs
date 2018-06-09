949a51cbfccee473bad22c959675d85439757c6f
Per Ivar Nerseth <perivar@nerseth.com>
2018-06-09 15:55:52 +0200

    sbanken transaction id debugging

--- AccountingServices/SBankenFactory.cs
+++ AccountingServices/SBankenFactory.cs
@@ -164,14 +164,14 @@ namespace AccountingServices
                     // Note, until Sbanken fixed their unique transaction Id issue, generate one ourselves
                     if (transactionId == null || !transactionId.HasValues || transactionId == JTokenType.Null)
                     {
-                        //string uniqueContentOriginal = $"{accountingDate}{interestDate}{transactionTypeText}{text}{amount}";
-                        //Console.WriteLine($"Unique Org: '{uniqueContentOriginal}'");
+                        string uniqueContentOriginal = $"{accountingDate}{interestDate}{transactionTypeText}{text}{amount}";
+                        Console.WriteLine($"Unique Org: '{uniqueContentOriginal}'");
 
                         string accountingDateNO = accountingDate.ToString("G", no);
                         string interestDateNO = interestDate.ToString("G", no);
                         string amountNO = amount.ToString("G", no);
                         string uniqueContent = $"{accountingDateNO}{interestDateNO}{transactionTypeText}{text}{amountNO}";
-                        //Console.WriteLine($"Unique New: '{uniqueContent}'");
+                        Console.WriteLine($"Unique New: '{uniqueContent}'");
 
                         string hashCode = Utils.CreateMD5(uniqueContent);
                         transactionId = hashCode;
