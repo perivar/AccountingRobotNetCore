3df758259f1ddca0ae9fe877244d9ba09746b04f
Per Ivar Nerseth <perivar@nerseth.com>
2018-06-11 08:15:16 +0200

    Fix dependencies

--- AccountingWebClient/Program.cs
+++ AccountingWebClient/Program.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Logging;
 
 namespace AccountingWebClient
 {
-   public class Program
+    public class Program
     {
         public static void Main(string[] args)
         {
@@ -31,7 +31,7 @@ namespace AccountingWebClient
                 e) Command-line arguments.
             4. Enable logging
             5. Integrates the Kestrel run with IIS             
-             */             
+             */
             WebHost.CreateDefaultBuilder(args)
                 .ConfigureAppConfiguration(builder =>
                 {
