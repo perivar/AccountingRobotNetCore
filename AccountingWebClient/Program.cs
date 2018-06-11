0087eaddf07cb67a36e472e01b48dd5e5872c576
Per Ivar Nerseth <perivar@nerseth.com>
2018-06-11 08:07:46 +0200

    add listen port and user secrets

--- AccountingWebClient/Program.cs
+++ AccountingWebClient/Program.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Logging;
 
 namespace AccountingWebClient
 {
-    public class Program
+   public class Program
     {
         public static void Main(string[] args)
         {
@@ -18,7 +18,26 @@ namespace AccountingWebClient
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
+
+            /*
+            The tasks that are performed by CreateDefaultBuilder is
+            1. Configures Kestrel as the web server.
+            2. Sets the content root to Directory.GetCurrentDirectory.
+            3. Loads optional configuration from
+                a) Appsettings.json
+                b) Appsettings.{Environment}.json.
+                c) User secrets when the app runs in the Development environment.
+                d) Environment variables
+                e) Command-line arguments.
+            4. Enable logging
+            5. Integrates the Kestrel run with IIS             
+             */             
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(builder =>
+                {
+                    // Add UserSecrets for both development and Production environment
+                    builder.AddUserSecrets<Startup>();
+                })
                 .UseStartup<Startup>()
                 .Build();
     }
